4d4a4f97f17647b75adb6a72af5484aab9c2e128
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-10-04 00:13:48 +1100

    added a second graph with dates

--- js/u-graph.js
+++ js/u-graph.js
@@ -2,6 +2,7 @@ var inputCrypto = document.getElementById("inputCrypto");
 var startDate = document.getElementById("inputStartDate");
 var endDate = document.getElementById("inputEndDate");
 var secSearchBtn = document.getElementById("searchBtn");
+var addHere = document.getElementById("newTetas");
 
 
 // CORS Proxy
@@ -48,16 +49,56 @@ async function displayGraph(data){
      return
     })
 }
-var addHere = document.getElementById("newTetas");
-function createChart(secondApi) {    
-    var chart = anychart.column(secondApi[0].prices); 
+function createChart(secondApi) { 
+    
     var div = document.createElement('div');
     div.id = 'container';
-    addHere.appendChild(div);
-    console.log(div);   
+    addHere.appendChild(div);  
+    
+    var chart = anychart.column(secondApi[0].prices); 
     chart.container('container');
     chart.draw();
+    
 
+
+    // canvas
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: secondApi[0].timestamps,
+            datasets: [{
+                label: 'Crypto Data',
+                data: secondApi[0].prices,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    
 };
 function resetState(){
     while (addHere.firstChild){
