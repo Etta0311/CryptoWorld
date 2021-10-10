2e5c3f8a06ef5cce98cc89ef1b5e9804a84f6dfb
HobbZ <zachobba@gmail.com>
2021-10-10 23:08:12 +1000

    Zac finished watchlist styling, changed bulma html to hide boxes until searched, cleaned up some watchlist JS

--- js/watchlist.js
+++ js/watchlist.js
@@ -14,6 +14,8 @@ var message = document.getElementById("message");
 var watchlist = document.getElementById("watchlist");
 var addtoWatchlistBtn = document.getElementById("addToWatchlistButton");
 var cryptoDescription = document.getElementById("aboutCrypto");
+var cardDisplay = document.getElementById("infoBox");
+var descBox = document.getElementById("infoBox2");
 
 //var savedItems = [];
 
@@ -33,19 +35,8 @@ coinCapApiKey.set("Authorization", "Bearer f3d5db36-3146-45e5-97fa-618fd419efc2"
 //____________INDEX PAGE CONTENT: SEARCH BAR AND CRYPTO INFO
 if($("body").data("title") === "indexPage") {
 
-  /*var savedItems = JSON.parse(localStorage.getItem("saved"));
-
-  //if nothing in search box and localStorage > 0
-  if(savedItems.length >= 0 && inputCrypto.textContent == "") {
-  for (let index = 0; index < savedItems.length; index++) {
-    var viewedCrypto = savedItems[index].Name;
-    console.log(viewedCrypto);
-    cryptoAPI(viewedCrypto.toLowerCase());
-  }
-}
-  } else if (inputCrypto.value != null) {
-    searchBtn.addEventListener('click', getCoin);
-  }*/
+cardDisplay.style.display = "none";
+descBox.style.display = "none";
 
 searchBtn.addEventListener('click', getCoin);
 
@@ -60,7 +51,7 @@ function getCoin(event) { //search coin after button click
   } else {
     message.textContent = "Please enter a valid cryptocurrency with no capital letters e.g. bitcoin";
     message.style.color = "red";
-    message.style.backgroundColor = "white";
+    //message.style.backgroundColor = "white";
     return;
   }
 }
@@ -82,6 +73,10 @@ async function cryptoAPI(coin){
 
 // this will assign api data to index.html
 function displayData(data){
+
+    cardDisplay.style.display = "block";
+    descBox.style.display = "block";
+
     cryptoName.textContent = data.data.name;
     cryptoTicker.textContent = "Ticker: " + data.data.symbol;
     cryptoRank.textContent = "Rank: " + data.data.rank;
@@ -188,49 +183,94 @@ addtoWatchlistBtn.addEventListener('click', addToWatchlist);
     var subheading = document.createElement("h6");
     watchlist.appendChild(subheading);
 
+    //Center watchlist text
+    watchlist.style.textAlign = "center";
+    watchlist.style.fontSize = "20px";
+
     var savedItems = JSON.parse(localStorage.getItem("saved")) || [];
+
+    var table = document.createElement("table"); //create table
+    //add table styles
+    table.classList.add("table","is-hoverable","is-fullwidth","is-narrow","is-size-6-mobile","has-text-left");
+
+    table.classList.add();
+
+    var thead = document.createElement("thead");
+    thead.classList.add("thead");
+    var tbody = document.createElement("tbody")
+    watchlist.appendChild(table);
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    //create top row 
+    var theader = document.createElement('tr');
+    //table header content
+    var thead1 = document.createElement("th");
+    var thead2 = document.createElement("th");
+    var thead3 = document.createElement("th");
+    var thead4 = document.createElement("th");
+    var thead5 = document.createElement("th");
+    var thead6 = document.createElement("th");
+
+    //assign text to header
+    thead1.textContent = "Name";
+    thead2.textContent = "Ticker";
+    thead3.textContent = "Unit AUD$";
+    thead4.textContent = "% Change";
+    //last 2 cells for buttons
+
+    //append header cells to header
+    theader.appendChild(thead1);
+    theader.appendChild(thead2);
+    theader.appendChild(thead3);
+    theader.appendChild(thead4);
+    theader.appendChild(thead5);
+    theader.appendChild(thead6);
+
+    thead.appendChild(theader);
 
       for (let index = 0; index < savedItems.length; index++) {
         subheading.textContent = savedItems.length + " added to watchlist";
 
-        var item = document.createElement("table"); //create table
-        item.setAttribute("data-watchlistIndex", index);
-        var newRow = item.insertRow(0);
+        var newRow = tbody.insertRow(0);
+        newRow.classList.add("tr");
 
         //create the required cells
         cell1 = newRow.insertCell(0);
         cell2 = newRow.insertCell(1);
         cell3 = newRow.insertCell(2);
         cell4 = newRow.insertCell(3);
+        cell5 = newRow.insertCell(4);
+        cell6 = newRow.insertCell(5);
 
         //add text from thirdApi.data data to cells
         cell1.textContent = savedItems[index].Name;
         cell2.textContent = savedItems[index].Ticker;
-        cell3.textContent = savedItems[index].USD$;
+        cell3.textContent = savedItems[index].USD$.replace("AUD: ", "");
         cell4.textContent = savedItems[index].change;
-
-        watchlist.appendChild(item);
         
         //create and append view coin button
         var viewCoinBtn = document.createElement('button');
         viewCoinBtn.textContent = "View";
-        viewCoinBtn.setAttribute("data-clicked", index);
-        item.appendChild(viewCoinBtn);
+        //view button styles
+        viewCoinBtn.classList.add("button","is-success", "is-fullwidth");
+
+        cell6.appendChild(viewCoinBtn);
 
         viewCoinBtn.addEventListener('click', function() {
-          //if (event.currentTarget) {
             //window.location.href = "index.html";
-        //}
         });
 
         //create and append remove coin button
         var removeCoinBtn = document.createElement('button');
         removeCoinBtn.textContent = "X";
-        item.appendChild(removeCoinBtn);
+        //button styling
+        removeCoinBtn.classList.add("button","is-danger", "is-fullwidth");
+
+        cell5.appendChild(removeCoinBtn);
 
           removeCoinBtn.addEventListener('click', function(event) {
             savedItems = JSON.parse(localStorage.getItem("saved"));
-
             subheading.textContent = savedItems.length;
 
             //force clear hack as event.currentarget was leaving one or two records in local storage sometimes
@@ -243,8 +283,8 @@ addtoWatchlistBtn.addEventListener('click', addToWatchlist);
             subheading.textContent = savedItems.length + " added to watchlist";
             localStorage.setItem("saved", JSON.stringify(savedItems));
 
-            //remove item from watchlist
-            event.currentTarget.parentNode.remove();
+            //remove record from watchlist
+            event.currentTarget.parentNode.parentNode.remove();
 
         }, false);
       }
